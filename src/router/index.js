e57458be3b75940784b338e4de3e371cb1d6e5d8
Sabbir <bsse1014@iit.du.ac.bd>
2021-04-20 10:16:31 +0600

    static destination and dynamic destination done

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
+
 const routes = [
   {
     path: "/",
@@ -16,6 +17,32 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/brazil",
+    name: "Brazil",
+    component: ()=> import(/* webpackChunkName: "brazil" */ "../views/Brazil")
+  },
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: ()=> import(/* webpackChunkName: "hawaii" */ "../views/Hawaii")
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: ()=> import(/* webpackChunkName: "panama" */ "../views/Panama")
+  },
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: ()=> import(/* webpackChunkName: "jamaica" */ "../views/Jamaica")
+  },
+  {
+    path: "/details/:id",
+    name: "DestinationDetails",
+    component: ()=> import(/* webpackChunkName: "details" */ "../views/DestinationDetails")
+  }
+
 ];
 
 const router = createRouter({
